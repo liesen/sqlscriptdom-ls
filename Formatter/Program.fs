c23c96358d6e367edf55c8e1c6ff7d383edff190
liesen <johan@liesen.se>
2024-08-15 19:06:57 +0200

    EditorConfig

--- Formatter/Program.fs
+++ Formatter/Program.fs
@@ -3,16 +3,21 @@ open System.IO
 open System.Web
 open Microsoft.SqlServer.TransactSql.ScriptDom
 open FsPretty.Rendering
+open EditorConfig.Core
 
 open Formatter
 
-
 [<EntryPoint>]
 let main args =
+    let editorconfig = EditorConfigParser().Parse(args.[0])
+    printfn "%A" editorconfig
+    printfn "%A" editorconfig.Properties
+
     let opts = SqlScriptGeneratorOptions()
-    opts.IndentationSize <- 2
+    opts.IndentationSize <- editorconfig.IndentSize.NumberOfColumns.GetValueOrDefault(2)
     opts.AlignClauseBodies <- false
     opts.AlignColumnDefinitionFields <- false
+
     let generator = Sql160ScriptGenerator(opts)
     let formatter = MySqlScriptGenerator(opts, generator)
 
