ed70147f6f27891c61b016193127ea19de14555d
liesen <johan@liesen.se>
2024-08-18 10:36:45 +0200

    Test for lite item alignment

--- Formatter.Tests/Trivial.fs
+++ Formatter.Tests/Trivial.fs
@@ -13,4 +13,10 @@ let tests =
           <| fun _ ->
               let input = new StringReader("SELECT * FROM [table]")
               let actual, errors = ppScript input
-              Expect.equal "Should equal" "SELECT *\nFROM [table]\n\n\n" actual ]
+              Expect.equal "Should equal" "SELECT *\nFROM [table]\n\n\n" actual
+
+          testCase "Align list items"
+          <| fun _ ->
+              let input = new StringReader("SELECT 1, 2, 3 FROM [table]")
+              let actual, errors = ppScript input
+              Expect.equal "Equal" "SELECT 1,\n  2,\n  3\nFROM [table]\n\n\n" actual ]
